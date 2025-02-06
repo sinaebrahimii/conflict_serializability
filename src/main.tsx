771f49d5db79cfcbcdc415c0482a0ced3ad9a34e
sina ebrahimi <sinaebrahimi31@gmail.com>
2025-02-06 21:49:09 +0330

    react router installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Define your routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Render your main App component here
+  },
+]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
